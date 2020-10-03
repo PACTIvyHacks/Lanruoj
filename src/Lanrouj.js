cbfd69dcb7e6589879c6fb380e7bbac28e2598a0
Palak Jain <palakj1@uci.edu>
2020-10-03 05:55:28 +0000

    added moments array,loosely defined addMoment

--- src/Lanrouj.js
+++ src/Lanrouj.js
@@ -1,16 +1,50 @@
 import React, { Component } from "react";
+import Momookies from "./Momookies"
  
 class Lanrouj extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      moments: []
+    };
+
+    this.addMoment = this.addMoment.bind(this);
+  }
+
+  addMoment(e) {
+    if (this._inputElement.value !== "") {
+      var newMoment = {
+        text: this._inputElement.value,
+        key: Date.now()
+      };
+   
+      this.setState((prevState) => {
+        return { 
+          moments: prevState.moments.concat(newMoment) 
+        };
+      });
+     
+      this._inputElement.value = "";
+    }
+     
+    console.log(this.state.moments);
+       
+    e.preventDefault();
+  }
+
   render() {
     return (
       <div className="lanroujMain">
         <div className="header">
-          <form>
-            <input placeholder="enter motivating moment">
+          <form onSubmit={this.addMoment}>
+            <input ref={(a) => this._inputElement = a} 
+                  placeholder="enter motivating moment">
             </input>
             <button type="submit">add</button>
           </form>
         </div>
+        <Momookies entries={this.state.moments}/>
       </div>
     );
   }
